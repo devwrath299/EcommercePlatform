15c0442a5e5a74ee027ef33af9859c0dd60d867b
Dev <devwrath.1@indiamart.com>
2024-05-25 16:55:09 +0530

    file_changes

---
+++ routes/route.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST()
+	incomingRoutes.POST()
+	incomingRoutes.POST()
+	incomingRoutes.GET()
+	incomingRoutes.GET()
+}
